80e4aca2bef0779f04e94d70d19039f0f5930ff0
cheoljundev <cheoljundev@gmail.com>
2024-07-19 16:19:15 +0900

    프로젝션(SELECT)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,31 +19,78 @@ public class JpaMain {
 
         try {
             Member member = new Member();
+            Team team = new Team();
+            Order order = new Order();
             member.setUsername("member1");
+            member.setTeam(team);
+
+            Address address = new Address();
+            address.setCity("Seoul");
+            address.setStreet("street");
+            address.setZipcode("12345");
+            order.setAddress(address);
 
             em.persist(member);
+            em.persist(team);
+            em.persist(order);
 
-            //TypedQuery,Query
-            TypedQuery<Member> query1 = em.createQuery("select  m from Member  m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select  m.username from Member  m", String.class);
-            Query query3 = em.createQuery("select  m.username, m.age from Member  m");
+            // 프로젝션(SELECT)
+            // 엔티티 프로젝션, 임베디드 프로젝션, 스칼라 프로젝션
 
+            em.flush();
+            em.clear();
 
-            // getResultList
-            List<Member> resultList = em.createQuery("select  m from Member  m", Member.class).getResultList();
-            for (Member m : resultList) {
-                System.out.println("member = " + m);
-            }
+            // 엔티티 프로젝션
+            // 찾은 결과는 영속성 컨텍스트에서 관리됨
 
-            // getSingleResult - 결과가 정확히 하나여야 한다.
-            Member singleResult = em.createQuery("select  m from Member  m where m.id = 1", Member.class).getSingleResult();
-            System.out.println("singleResult = " + singleResult);
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
 
-            // setParameter
-            Member singleResult1 = em.createQuery("select  m from Member  m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("singleResult1 = " + singleResult1);
+            Member findMember = result.get(0);
+            findMember.setAge(20);
+            System.out.println("findMember = " + findMember);
+
+            // 엔티티 프로젝션
+
+            // List<Team> resultList1 = em.createQuery("select m.team from Member m", Team.class)
+            //        .getResultList(); -> 좋지 않음. 예측불가
+
+            List<Team> result2 = em.createQuery("select t from Member m join m.team t", Team.class)
+                    .getResultList(); // 예측 가능
+
+            Team resultTeam = result2.get(0);
+
+            // 임베디드 타입 프로젝션
+            List<Address> result3 = em.createQuery("select o.address from Order o", Address.class)
+                    .getResultList();
+            Address findAddress = result3.get(0);
+
+            // 스칼라 프로젝션
+//            List resultList = em.createQuery("select distinct m.username, m.age from Member m")
+//                    .getResultList();
+//
+//            Object[] sResult = (Object[]) resultList.get(0);
+//            System.out.println("username = " + sResult[0]);
+//            System.out.println("age = " + sResult[1]);
+
+            // 스칼라 + 제네릭
+
+//            List<Object[]> resultList = em.createQuery("select distinct m.username, m.age from Member m")
+//                    .getResultList();
+//
+//            Object[] sResult = resultList.get(0);
+//            System.out.println("username = " + sResult[0]);
+//            System.out.println("age = " + sResult[1]);
+
+            // 스칼라 + DTO
+
+            List<MemberDto> resultList = em.createQuery("select distinct new jpql.MemberDto(m.username, m.age) from Member m", MemberDto.class)
+                    .getResultList();
+
+            MemberDto memberDto = resultList.get(0);
+            System.out.println("username = " + memberDto.getUsername());
+            System.out.println("age = " + memberDto.getAge());
+
 
             tx.commit();
         } catch (Exception e) {
